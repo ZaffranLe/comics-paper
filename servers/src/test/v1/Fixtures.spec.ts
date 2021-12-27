84bc61c6f05b2beff5f11c40c77c2bf35f20f163
Player Nguyên <daudaua3782@gmail.com>
2021-12-27 23:43:12 +0700

    add test unit

---
+++ servers/src/test/v1/Fixtures.spec.ts
@@ -0,0 +1,9 @@
+export async function mochaGlobalSetup() {
+  // this.server = await startSomeServer({port: process.env.TEST_PORT});
+  // console.log(`server running on port ${this.server.port}`);
+}
+
+export async function mochaGlobalTeardown() {
+  // await this.server.stop();
+  // console.log('server stopped!');
+}
---
+++ servers/src/test/v1/Test.spec.ts
@@ -0,0 +1,8 @@
+import * as chai from "chai";
+const expect = chai.expect;
+
+describe(`v1: Test engine`, () => {
+  it(`Check for expecting true to be true`, () => {
+    expect(true).to.be.true;
+  });
+});
