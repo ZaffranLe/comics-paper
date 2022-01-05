2a227bba1c9a080715952dcbbdc08cddfde335ac
Tung Le <biprono12@gmail.com>
2022-01-06 00:42:05 +0700

    improve home page

--- renders/web/src/App.js
+++ renders/web/src/App.js
@@ -1,5 +1,9 @@
 import React from "react";
 import AppRouter from "./router";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faAlignJustify, faBell, faTimes, faTrophy } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faBell, faAlignJustify, faTimes, faTrophy);
 
 function App() {
     return (
