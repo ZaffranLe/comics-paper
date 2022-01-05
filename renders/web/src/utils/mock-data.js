1f83376b5f865a87e449dbf3c3f4d012e4988d62
Tung Le <biprono12@gmail.com>
2022-01-06 00:41:04 +0700

    add mock data and common functions

--- renders/web/src/utils/mock-data.js
+++ renders/web/src/utils/mock-data.js
@@ -1,3 +1,29 @@
-export {
-    
-}
+const mangaList = [
+    {
+        url: "mock-url",
+        title: "mock-title",
+        img: "mock-img",
+    },
+    {
+        url: "mock-url",
+        title: "mock-title",
+        img: "mock-img",
+    },
+    {
+        url: "mock-url",
+        title: "mock-title",
+        img: "mock-img",
+    },
+    {
+        url: "mock-url",
+        title: "mock-title",
+        img: "mock-img",
+    },
+    {
+        url: "mock-url",
+        title: "mock-title",
+        img: "mock-img",
+    },
+];
+
+export { mangaList };
