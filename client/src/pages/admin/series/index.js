11079926a74d6ed515c06b3ba508f09e36348176
ZaffranLe <stungle154@gmail.com>
2022-02-18 12:10:53 +0700

    update admin serie page

--- client/src/pages/admin/series/index.js
+++ client/src/pages/admin/series/index.js
@@ -1,9 +1,22 @@
 import React from "react";
 import * as comicApi from "../../../utils/api/comics";
-import { BookThumbnail } from "../../../components";
+import { BookThumbnail, ConfirmModal } from "../../../components";
+import { toast } from "react-toastify";
+import { v1 as uuidv1 } from "uuid";
+import UpdateSerie from "./update";
 
 function Series() {
     const [series, setSeries] = React.useState([]);
+    const [updateModal, setUpdateModal] = React.useState(false);
+    const [updateSerie, setUpdateSerie] = React.useState(null);
+    const [randomKey, setRandomKey] = React.useState(0);
+    const [confirmModal, setConfirmModal] = React.useState({
+        content: "",
+        onClose: () => {},
+        onConfirm: () => {},
+        open: false,
+        loading: false,
+    });
 
     const fetchSeries = async () => {
         try {
@@ -18,10 +31,68 @@ function Series() {
         fetchSeries();
     }, []);
 
+    const handleOpenUpdateModal = (selectedBookTag = null) => {
+        setRandomKey(uuidv1());
+        setUpdateModal(true);
+        if (selectedBookTag) {
+            setUpdateSerie(selectedBookTag);
+        }
+    };
+
+    const handleCloseUpdateModal = () => {
+        setUpdateModal(false);
+        if (updateSerie) {
+            setUpdateSerie(null);
+        }
+    };
+
+    const handleSave = async (keyword) => {
+        // try catch is in UpdateSerie
+        if (updateSerie) {
+            // await bookTagApi.updateSerie(updateSerie.id, keyword);
+            toast.success("Cập nhật truyện thành công");
+        } else {
+            // await bookTagApi.createBookTag(keyword);
+            toast.success("Tạo truyện thành công");
+        }
+        await fetchSeries();
+    };
+
+    const handleDeleteSerie = (serie) => {
+        setConfirmModal({
+            content: `Bạn có chắc chắn muốn xóa truyện '${serie.name}' không?`,
+            open: true,
+            loading: false,
+            onClose: () => {
+                setConfirmModal({ ...confirmModal, open: false });
+            },
+            onConfirm: async () => {
+                setConfirmModal({ ...confirmModal, loading: true });
+                try {
+                    // await bookTagApi.deleteBookTag(tag.id);
+                    toast.success("Xóa truyện thành công");
+                    // await fetchBookTags();
+                } catch (e) {
+                    console.error(e);
+                    toast.error("Xóa truyện thất bại, vui lòng thử lại sau.");
+                } finally {
+                    setConfirmModal({
+                        ...confirmModal,
+                        loading: false,
+                        open: false,
+                    });
+                }
+            },
+        });
+    };
+
     return (
         <>
             <div className="text-2xl font-bold">Danh sách truyện</div>
-            <button className="bg-transparent hover:underline hover:text-indigo-500 font-semibold px-2 py-1 mt-4">
+            <button
+                onClick={() => handleOpenUpdateModal()}
+                className="bg-transparent hover:underline hover:text-indigo-500 font-semibold px-2 py-1 mt-4"
+            >
                 Tạo mới
             </button>
             <div className="mt-8 grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-8">
@@ -31,6 +102,14 @@ function Series() {
                     </div>
                 ))}
             </div>
+            <UpdateSerie
+                key={randomKey}
+                open={updateModal}
+                onClose={handleCloseUpdateModal}
+                onSave={handleSave}
+                updateSerie={updateSerie}
+            />
+            <ConfirmModal {...confirmModal} />
         </>
     );
 }
---
+++ client/src/pages/admin/series/update.js
@@ -0,0 +1,90 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { toast } from "react-toastify";
+import { Modal } from "../../../components";
+import { classNames } from "../../../utils/common";
+
+function UpdateBookTag({ open, onClose, onSave, updateBookTag }) {
+    const [keyword, setKeyword] = React.useState("");
+    const [loading, setLoading] = React.useState(false);
+
+    React.useEffect(() => {
+        if (updateBookTag) {
+            setKeyword(updateBookTag.keyword);
+        }
+    }, [updateBookTag]);
+
+    const handleChangeKeyword = (e) => {
+        setKeyword(e.target.value);
+    };
+
+    const handleSave = async () => {
+        try {
+            setLoading(true);
+            await onSave(keyword);
+            onClose();
+        } catch (e) {
+            toast.error(
+                e.response?.data?.error?.message ||
+                    "Cập nhật thể loại truyện thất bại, vui lòng thử lại sau."
+            );
+            console.error(e);
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    const disableSaveBtn =
+        loading || (updateBookTag && keyword === updateBookTag.keyword);
+
+    return (
+        <>
+            <Modal dimmer open={open} onClose={onClose}>
+                <div className="w-1/3 bg-white border rounded-xl p-4">
+                    <div className="text-xl font-bold">
+                        {updateBookTag
+                            ? "Cập nhật thể loại truyện"
+                            : "Tạo thể loại truyện mới"}
+                    </div>
+                    <div className="mt-4 mb-4">
+                        <label>Từ khóa</label>
+                        <input
+                            className="input w-full"
+                            value={keyword}
+                            onChange={handleChangeKeyword}
+                        />
+                    </div>
+                    <div className="text-right">
+                        <button
+                            onClick={handleSave}
+                            className={classNames(
+                                !disableSaveBtn &&
+                                    "hover:bg-indigo-400 hover:text-white",
+                                "ring-2 ring-indigo-400 text-indigo-400 font-semibold py-2 px-4 rounded-full mr-2"
+                            )}
+                            disabled={disableSaveBtn}
+                        >
+                            {loading ? (
+                                <FontAwesomeIcon
+                                    icon="spinner"
+                                    spin
+                                    fixedWidth
+                                />
+                            ) : (
+                                "Lưu"
+                            )}
+                        </button>
+                        <button
+                            onClick={onClose}
+                            className="ring-2 ring-gray-500 text-gray-500 hover:bg-gray-500 hover:text-white font-semibold py-2 px-4 rounded-full ml-2"
+                        >
+                            Đóng
+                        </button>
+                    </div>
+                </div>
+            </Modal>
+        </>
+    );
+}
+
+export default UpdateBookTag;
