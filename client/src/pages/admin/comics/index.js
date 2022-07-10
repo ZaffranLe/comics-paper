73edfe493cc9f4670fe78e8c8d4601ca519dea36
TungNWS <tungls@newwave.vn>
2022-07-10 19:31:46 +0700

    wip: add admin comic detail page

--- client/src/pages/admin/comics/index.js
+++ client/src/pages/admin/comics/index.js
@@ -171,8 +171,11 @@ function Comics() {
             </button>
             <div className="mt-8 grid grid-cols-2 sm:grid-cols-4 lg:grid-cols-5 xl:grid-cols-7 2xl:grid-cols-8 gap-8">
                 {comics.map((_comic) => (
-                    <div key={_comic.id} onClick={(e) => handleOpenContextMenu(e, _comic)}>
-                        <ComicThumbnail comic={_comic} url={`#`} />
+                    <div
+                        key={_comic.id}
+                        // onClick={(e) => handleOpenContextMenu(e, _comic)}
+                    >
+                        <ComicThumbnail comic={_comic} url={`/dashboard/comics/${_comic.id}`} />
                     </div>
                 ))}
             </div>
