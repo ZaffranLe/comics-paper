ce6b4a0a0965b9444762baaf7abaacf0a58425ad
Player Nguyên <daudaua3782@gmail.com>
2023-03-02 13:56:41 +0700

    add: cross-env dependency, check .env scripts

---
+++ servers/scripts/has-env.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+/**
+ * Check if the .env file is existed or not
+ */
+if (!fs.existsSync(".env")) {
+  throw new Error("The .env file not found. Make sure you set it up first.");
+}
